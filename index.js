4ae73c3652a01fac217f9b948e991197972ead07
Chirag <chiragadve4@gmail.com>
2024-09-24 23:39:49 +0530

    git commit

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const urlRoute = require("./routes/url");
 const app = express();
 
 const PORT = 8001;
+
+app.use("/url", urlRoute);
 
 app.listen(8001, () => console.log(`Server Started at port: ${PORT}`));
